f0eb3555506bdeee603cb8b77784377360d16b0b
Shubham Kumar <shubham@glassfrog.design>
2025-01-31 09:58:38 +0530

    changes update

---
+++ src/app/about-us/layout.js
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: " About Us - Glassfrog Technologies",
+  description: "Glassfrog offers specialised solutions in Website Development, Mobile Application, Digital Marketing, SEO, SMM, and more. Learn everything about us here",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="canonical" href="http://glassfrogtech.co.uk/" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
