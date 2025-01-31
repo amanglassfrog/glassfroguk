d906944b7d5fdac32304750c88ebb172d48e6f43
Shubham Kumar <shubham@glassfrog.design>
2025-01-31 16:50:08 +0530

    new changes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -20,6 +20,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-J69HCJ44DZ"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-J69HCJ44DZ');
+              `,
+          }}
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
