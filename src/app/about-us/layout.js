e0b54a11ad4126fab95c464639afd8d5ef157846
Shubham Kumar <shubham@glassfrog.design>
2025-01-31 09:59:45 +0530

    new changes

--- src/app/about-us/layout.js
+++ src/app/about-us/layout.js
@@ -19,9 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="canonical" href="http://glassfrogtech.co.uk/" />
-      </head>
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
