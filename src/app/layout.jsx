36f494b0f0b60d6d156bf70fceb8ac05de365e47
Shubham Kumar <shubham@glassfrog.design>
2025-02-03 10:16:03 +0530

    changes update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -35,6 +35,18 @@ export default function RootLayout({ children }) {
               `,
           }}
         ></script>
+        <script async src="https://www.clarity.ms/tag/q3vsh10s8d"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "q3vsh10s8d");
+            `,
+          }}
+        ></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
